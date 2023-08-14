e998902fd25bdd75b29d5a207658d2fb6799582e
hyangsso <sohyang8946@gmail.com>
2023-08-14 16:39:08 +0900

    Fix: center_type 저장 안 되는 버그 수정 완료

--- airflow/dags/dag1.py
+++ airflow/dags/dag1.py
@@ -3,7 +3,6 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime, timedelta
 from airflow.models import Variable
-from dotenv import load_dotenv
 import os
 import json
 import xmltodict
@@ -16,7 +15,7 @@ logging.basicConfig(level=logging.DEBUG)
 # DAG 설정
 default_args = {
     'start_date': datetime(2023, 8, 9),
-    'retries': 1,
+    # 'retries': 1,
     'timezone': 'Asia/Seoul',
     'retry_delay': timedelta(minutes=5)
 }
@@ -29,20 +28,33 @@ dag = DAG(
 
 # API 호출
 def call_api(url, **kwargs):
-    load_dotenv()
     servicekey = Variable.get('SERVICEKEY')
     params = {'serviceKey': servicekey, 'pageNo' : '1', 'numOfRows' : '9999' }
-
+    
     response = requests.get(url, params=params)
     xmlString = response.text
     jsonString = xmltodict.parse(xmlString)
+        
     data = jsonString['response']['body']['items']['item']
-    kwargs['ti'].xcom_push(key='list_info_data', value=data)
-    return data
-
+    
+    if url.split('/')[-1] == 'getEgytListInfoInqire': #응급의료기관
+        [duty.update({'center_type': 0}) for duty in data]
+    elif url.split('/')[-1] == 'getStrmListInfoInqire': #외상센터
+        [duty.update({'center_type': 1}) for duty in data]
+    else: 
+        [duty.update({'center_type': -1}) for duty in data] # 슬랙 메세지 호출 기능 추가 예정
+        
+    if kwargs['ti'].task_id == 'call_api_task_0':
+        kwargs['ti'].xcom_push(key='list_info_data_0', value=data)
+    elif kwargs['ti'].task_id == 'call_api_task_1':
+        kwargs['ti'].xcom_push(key='list_info_data_1', value=data)
+    
 # 데이터 적재
 def load_data_to_rds(**kwargs):
-    data = kwargs['ti'].xcom_pull(key='list_info_data')
+    data_0 = kwargs['ti'].xcom_pull(key='list_info_data_0')
+    data_1 = kwargs['ti'].xcom_pull(key='list_info_data_1')
+        
+    data = data_0 + data_1
     
     host = Variable.get('HOST')
     database = Variable.get('DATABASE')
@@ -69,9 +81,10 @@ def load_data_to_rds(**kwargs):
         phpid = x.get('phpid', '')
         wgs_84_lat = x.get('wgs84Lat', '')
         wgs_84_lon = x.get('wgs84Lon', '')
+        center_type = x.get('center_type', '')
 
         query = f"INSERT INTO HOSPITAL_BASIC_INFO (hpid, phpid, duty_emcls, duty_emcls_name, duty_addr, duty_name, duty_tel1, duty_tel3, wgs_84_lon, wgs_84_lat, center_type)" \
-                f" VALUES ('{hpid}', '{phpid}', '{duty_emcls}', '{duty_emcls_name}', '{duty_addr}', '{duty_name}', '{duty_tel1}', '{duty_tel3}', '{wgs_84_lon}', '{wgs_84_lat}', 1)"
+                f" VALUES ('{hpid}', '{phpid}', '{duty_emcls}', '{duty_emcls_name}', '{duty_addr}', '{duty_name}', '{duty_tel1}', '{duty_tel3}', '{wgs_84_lon}', '{wgs_84_lat}', '{center_type}')"
         print(query)
         cursor.execute(query)
     conn.commit()  
@@ -82,13 +95,18 @@ start_task = DummyOperator(
 )
 
 # 각 API 호출 태스크 생성
-list_api_urls = [
-    'http://apis.data.go.kr/B552657/ErmctInfoInqireService/getEgytListInfoInqire',
-    'http://apis.data.go.kr/B552657/ErmctInfoInqireService/getStrmListInfoInqire'
+basic_list_api_urls = [
+    'http://apis.data.go.kr/B552657/ErmctInfoInqireService/getEgytListInfoInqire', # 응급의료기관
+    'http://apis.data.go.kr/B552657/ErmctInfoInqireService/getStrmListInfoInqire' # 외상센터
+]
+
+detail_list_api_urls = [
+    'http://apis.data.go.kr/B552657/ErmctInfoInqireService/getEgytBassInfoInqire',
+    'http://apis.data.go.kr/B552657/ErmctInfoInqireService/getStrmBassInfoInqire'
 ]
 
 api_tasks = []
-for i, api_url in enumerate(list_api_urls):
+for i, api_url in enumerate(basic_list_api_urls):
     api_task = PythonOperator(
         task_id=f'call_api_task_{i}',
         python_callable=call_api,
@@ -106,5 +124,10 @@ load_to_rds_task = PythonOperator(
     dag=dag,
 )
 
+for i, api_url in enumerate(basic_list_api_urls):
+    api_task = PythonOperator(
+        
+        
+    )
 # 의존성 설정
-start_task >> api_tasks >> load_to_rds_task
+start_task >> api_tasks >> load_to_rds_task 
