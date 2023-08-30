637a5f6ef6dc3c88b5815c921cb31029a9ad867c
hyangsso <sohyang8946@gmail.com>
2023-08-31 01:34:21 +0900

    Feat: sqlalchemy 추가

--- airflow/dags/s3_daily_data_to_redshift.py
+++ airflow/dags/s3_daily_data_to_redshift.py
@@ -1,91 +1,145 @@
 from datetime import datetime, timedelta
 from module.util.connector.redshfit import ConnectRedshift
 from module.util.connector.s3 import ConnectS3
+import pandas as pd
+import io
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.models import Variable
+from airflow.operators.empty import EmptyOperator
+from airflow.utils.task_group import TaskGroup
 
 # DAG 설정
 default_args = {
     'start_date': datetime(2023, 8, 28),
-    # 'retries': 1,
     'timezone': 'Asia/Seoul',
     'retry_delay': timedelta(minutes=5)
 }
 
-dag = DAG(
+def ReadS3file(info_type, **kwargs):
+    
+    current_task_name = kwargs['task_instance'].task_id
+
+    s3_client = ConnectS3()
+    bucket_name = 'de-5-1'
+    file_path = f'batch/year=2023/month=08/day=30/{info_type}_2023-08-30.csv'
+    csv_temp_file = 'temp_s3_file.csv'
+    s3_client.download_file(bucket_name, file_path, csv_temp_file)
+
+    df = pd.read_csv(csv_temp_file)
+    # , 제거
+    if df.columns[0] != 'hpid':
+        dfresult = df.drop(df.columns[0], axis=1)
+    else: 
+        dfresult = df
+    
+    # detail info의 경우, hpid, hvec, dt 컬럼만 redshift에 적재
+    if info_type == 'detail':
+        dfresult_bi = dfresult[['hpid', 'hvec', 'dt']]
+    else:
+        dfresult_bi = dfresult
+    
+    kwargs['ti'].xcom_push(key='bi_dataframe', value=dfresult_bi)    
+    kwargs['ti'].xcom_push(key='s3_dataframe', value=dfresult)
+    kwargs['ti'].xcom_push(key='file_path', value=file_path)
+
+def LoadToS3(**kwargs):
+    s3_client = ConnectS3()
+    df = kwargs['ti'].xcom_pull(key='s3_dataframe')
+    path = kwargs['ti'].xcom_pull(key='file_path')
+    print(path)
+    
+    csv_buffer = io.BytesIO()
+    df.to_csv(csv_buffer, index=False)
+    csv_buffer.seek(0)
+    
+    s3_client.upload_fileobj(csv_buffer, 'de-5-1', path)
+    
+def LoadToReshift(info_type, **kwargs):
+    dfresult_bi = kwargs['ti'].xcom_pull(key='bi_dataframe')
+    
+    engine, conn = ConnectRedshift()
+    # DataFrame을 Redshift 테이블로 적재
+    table_name = f'hospital_{info_type}_info'
+    schema_name = 'public'
+
+    # DataFrame을 Redshift에 적재
+    dfresult_bi.to_sql(name=table_name, con=engine, schema=schema_name, if_exists='replace', index=False)
+
+    conn.close()
+
+with DAG(
     's3_daily_data_to_redshift',
     default_args=default_args,
     schedule_interval=timedelta(days=1)
-)
+) as dag:
 
-def copy_redshift_table_with_csv(table_name, bucket_name, file_name):
-    conn, cursor = ConnectRedshift()
-    
-    aws_access_key_id = Variable.get('AWS_ACCESS_ID')
-    aws_secret_access_key = Variable.get('AWS_SECRET_ACCESS_KEY')
-
-    try:
-        delete_query = f'delete from {table_name};'
-
-        copy_query = f"""
-            COPY {table_name}
-            FROM 's3://{bucket_name}/{file_name}'
-            CREDENTIALS 'aws_access_key_id={aws_access_key_id};aws_secret_access_key={aws_secret_access_key}'
-            FORMAT AS CSV
-            IGNOREHEADER 1;
-        """
-        cursor.execute(delete_query)
-        cursor.execute(copy_query)
-
-        conn.commit()
-    except Exception as e:
-        print(e)
-        conn.rollback()
-    finally:
-        conn.close()
-
-
-def get_latest_file_from_s3(**context):
-    now = datetime.now()
-    now += timedelta(hours=9)
-    today = str(now.year)+str(now.month).zfill(2)+str(now.day-4)
-
-    bucket_name = context['params']['bucket_name']
-
-    s3_client = ConnectS3()
-
-    obj_list = s3_client.list_objects(Bucket=bucket_name)
-    contents_list = obj_list['Contents']
-
-    key_list = [x['Key'] for x in contents_list if f'test/{today}' in x['Key']]
-
-    context['ti'].xcom_push(key='latest_file_names', value=sorted(key_list))
-
-
-def update_data_to_redshift(**context):
-    file_names = context['ti'].xcom_pull(key='latest_file_names')
-    bucket_name = context['params']['bucket_name']
-    print(file_names)
-    copy_redshift_table_with_csv('HOSPITAL_BASIC_INFO', bucket_name, file_names[0])
-    copy_redshift_table_with_csv('HOSPITAL_DETAIL_INFO', bucket_name, file_names[1])
-
-
-get_latest_file_name = PythonOperator(
-        task_id='get_latest_file_name',
-        python_callable=get_latest_file_from_s3,
-        params={'bucket_name': 'de-5-1'},
-        provide_context=True,
+    start_task = EmptyOperator(
+        task_id = 'start',
         dag=dag
     )
 
-update_data_to_redshift = PythonOperator(
-        task_id='update_data_to_redshift',
-        python_callable=update_data_to_redshift,
-        params={'bucket_name': 'de-5-1'},
-        provide_context=True,
+    with TaskGroup(group_id='load_basic_info', dag=dag) as load_basic_info :
+        
+        info_type = 'basic'
+        
+        read_s3_file = PythonOperator(
+            task_id = 'read_s3_file_task',
+            python_callable=ReadS3file,
+            op_args=[info_type],
+            provide_context=True,
+            dag=dag
+        )
+
+        load_new_file_s3 = PythonOperator(
+            task_id = 'load_to_s3_task',
+            python_callable=LoadToS3,
+            provide_context=True,
+            dag=dag
+        )
+
+        load_bi_file_redshift = PythonOperator(
+            task_id = 'load_to_redshift_task',
+            python_callable=LoadToReshift,
+            op_args=[info_type],
+            provide_context=True,
+            dag=dag
+        )
+        
+        read_s3_file >> load_new_file_s3 >> load_bi_file_redshift
+
+    with TaskGroup(group_id='load_detail_info', dag=dag) as load_detail_info :
+        
+        info_type = 'detail'
+
+        read_s3_file = PythonOperator(
+            task_id = 'read_s3_file_task',
+            python_callable=ReadS3file,
+            op_args=[info_type],
+            provide_context=True,
+            dag=dag
+        )
+
+        load_new_file_s3 = PythonOperator(
+            task_id = 'load_to_s3_task',
+            python_callable=LoadToS3,
+            provide_context=True,
+            dag=dag
+        )
+
+        load_bi_file_redshift = PythonOperator(
+            task_id = 'load_to_redshift_task',
+            python_callable=LoadToReshift,
+            op_args=[info_type],
+            provide_context=True,
+            dag=dag
+        )
+
+        read_s3_file >> load_new_file_s3 >> load_bi_file_redshift
+
+    end_task = EmptyOperator(
+        task_id = 'end',
         dag=dag
     )
 
-get_latest_file_name >> update_data_to_redshift
+start_task >> [load_basic_info, load_detail_info] >> end_task
