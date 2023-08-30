d9c9499b12ea94f3de707707b62c92c75a2a6344
hyangsso <sohyang8946@gmail.com>
2023-08-31 01:34:08 +0900

    Feat: redshift 연결 엔진 변경

--- airflow/dags/module/util/connector/redshfit.py
+++ airflow/dags/module/util/connector/redshfit.py
@@ -1,7 +1,24 @@
-from airflow.providers.postgres.hooks.postgres import PostgresHook
+import psycopg2
+from airflow.models import Variable
+from sqlalchemy import create_engine
 
-def ConnectRedshift(autocommit=True):
-    hook = PostgresHook(postgres_conn_id='aws_redshift_conn')
-    conn = hook.get_conn()
+def ConnectRedshift():
+    # Redshift connection info
+    host = Variable.get("REDSHIFT_HOST")
+    dbname = Variable.get("REDSHIFT_DBNAME")
+    user = Variable.get("REDSHIFT_USER")
+    password = Variable.get("REDSHIFT_PASSWORD")
+    port = Variable.get("REDSHIFT_PORT")
+
+    # Redshift connection
+    conn = psycopg2.connect(
+        host=host,
+        dbname=dbname,
+        user=user,
+        password=password,
+        port=port
+    )
     
-    return conn, conn.cursor()
+    engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}')
+
+    return engine, conn
